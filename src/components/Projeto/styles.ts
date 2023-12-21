87e00da31453721aeba717199104d2ee5ba86425
Anderson Casimiro <cassgkn@hotmail.com>
2023-12-21 17:38:24 -0300

    Criando a seção de projetos e o componente do projeto

---
+++ src/components/Projeto/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  border: 1px solid #c1c1c1;
+  padding: 16px;
+  border-radius: 7px;
+`
+
+export const LinkBotao = styled.a`
+  color: #fff;
+  font-size: 14px;
+  background-color: #4476be;
+  text-decoration: none;
+  padding: 8px;
+  margin-top: 24px;
+  display: inline-block;
+  border-radius: 7px;
+  cursor: pointer;
+`
